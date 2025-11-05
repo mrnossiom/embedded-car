024c50d5178d09ef20f712e40d4b49926aef52e1
Milo Moisson <milo@wiro.world>
2025-11-05 09:45:46 +0100

    update nix flake

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
 
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
@@ -8,48 +8,60 @@
     };
   };
 
-  outputs = { self, nixpkgs, rust-overlay }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+    }:
     let
       inherit (nixpkgs.lib) genAttrs;
 
-      forAllSystems = genAttrs [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ];
+      forAllSystems = genAttrs [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
       forAllPkgs = function: forAllSystems (system: function pkgs.${system});
 
-      pkgs = forAllSystems (system: (import nixpkgs {
-        inherit system;
-        overlays = [ (import rust-overlay) ];
-      }));
+      pkgs = forAllSystems (
+        system:
+        import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        }
+      );
     in
     {
-      formatter = forAllPkgs (pkgs: pkgs.nixpkgs-fmt);
+      formatter = forAllPkgs (pkgs: pkgs.nixfmt-tree);
 
-      devShells = forAllPkgs (pkgs:
-        with pkgs.lib;
+      devShells = forAllPkgs (
+        pkgs:
         let
           file-rust-toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
           rust-toolchain = file-rust-toolchain.override { extensions = [ "rust-analyzer" ]; };
         in
         {
-          default = pkgs.mkShell rec {
-            nativeBuildInputs = with pkgs; [
+          default = pkgs.mkShell {
+            packages = with pkgs; [
               pkg-config
               rust-toolchain
-              act
               just
 
-              probe-rs
+              probe-rs-tools
               fritzing
               usbutils
               minicom
+
+              dbus
+              systemd
             ];
-            buildInputs = with pkgs; [ dbus systemd ];
 
             RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
-            LD_LIBRARY_PATH = makeLibraryPath buildInputs;
 
-            RUST_LOG = "";
             DEFMT_LOG = "debug";
           };
-        });
+        }
+      );
     };
 }
